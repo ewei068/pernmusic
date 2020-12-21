dac6ae1fc58ab61636b0914a57444141ad5d93df
Elvis Wei <ewei068@berkeley.edu>
2020-12-20 18:45:19 -0600

    basic server + client finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import List from "./List.js";
 
--- client/src/List.js
+++ client/src/List.js
@@ -3,33 +3,34 @@ import { Dropdown, DropdownButton } from "react-bootstrap";
 
 const List = () => {
   const [users, setUsers] = useState([]);
+  const [likes, setLikes] = useState([]);
 
   const getUsers = async () => {
-    try {
-      const response = await fetch("http://localhost:8000/users");
-      const jsonData = await response.json();
-      setUsers(jsonData);
-    } catch (err) {
-      console.error(err.message);
-    }
+    const response = await fetch("http://localhost:8000/users");
+    const jsonData = await response.json();
+    setUsers(jsonData);
   };
 
   const getLikes = async (id) => {
-    console.log(id);
+    const response = await fetch("http://localhost:8000/likes/" + id);
+    const jsonData = await response.json();
+    setLikes(jsonData);
+    //console.log(jsonData);
   }
 
   useEffect(() => {
     getUsers();
   }, []);
+  // console.log(likes);
 
-  console.log(users);
+  //console.log(users);
 
   return (
     <Fragment>
-      <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+      <DropdownButton id="dropdown-basic-button" title="Users">
       {
         users.map(user => (
-          <Dropdown.Item onClick = {() => getLikes(user.user_id)}>{user.user_id}</Dropdown.Item>
+          <Dropdown.Item onClick = {() => getLikes(user.user_id)}>{user.user_last_name + ', ' + user.user_first_name}</Dropdown.Item>
         ))
       }
       </DropdownButton>
@@ -40,19 +41,10 @@ const List = () => {
           </tr>
         </thead>
         <tbody>
-          {todos.map(todo => (
-            <tr key={todo.todo_id}>
-              <td>{todo.description}</td>
+          {likes.map(like => (
+            <tr key={likes.indexOf(like)}>
               <td>
-                <EditTodo todo={todo} />
-              </td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => deleteTodo(todo.todo_id)}
-                >
-                  Delete
-                </button>
+                {like.fan_of_music_group}
               </td>
             </tr>
           ))}
